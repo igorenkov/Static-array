b72c0e5676812d6447559222f6dcc8940e774a2e
Мануэль Горенков <igorenkov54@mail.ru>
2019-04-16 02:52:51 +0700

    Change function

--- Статический массив/Статический массив.c
+++ Статический массив/Статический массив.c
@@ -4,26 +4,49 @@
 
 int arr[MAX];
 
-//Вычисление максимума/минимума
-void minax(int len) {
+//Вычисление максимума
+int max(int len) {
 	//Указаетель на глобальный массив
 	int* parr = arr;
-	int maxt = *parr, mint = *parr;
+	int maxt = *parr;
 
 	//Работа с массивом
 	for (int i = 1; i < len; i++) {
 		if (*(parr + i) > maxt) {
 			maxt = *(parr + i);
 		}
-		else if (*(parr + i) < mint) {
+	}
+	return maxt;
+}
+
+//Вычисление минимума
+int min(int len) {
+	//Указаетель на глобальный массив
+	int* parr = arr;
+	int mint = *parr;
+
+	//Работа с массивом
+	for (int i = 1; i < len; i++) {
+		if (*(parr + i) < mint) {
 			mint = *(parr + i);
 		}
 	}
-	printf("Maximum - %d\nMinimum - %d\n", mint, maxt);
+	return mint;
 }
 
-//Вычисление среднего и отклонения
-void meams(int len) {
+//Вычисление среднего
+float mean(int len) {
+	float mean = 0;
+
+	//Расчёт среднего
+	for (int i = 0; i < len; i++) {
+		mean = mean + ((float)arr[i] / len);
+	}
+	return mean;
+}
+
+//Вычисление отклонения
+float RMS(int len) {
 	float sigma = 0, mean = 0;
 	//sigma - квадрат отклонения
 
@@ -35,7 +58,7 @@ void meams(int len) {
 	for (int i = 0; i < len; i++) {
 		sigma = sigma + ((mean - arr[i])*(mean - arr[i]) / len);
 	}
-	printf("Mean - %f\nRMS - %f\n", mean, sqrt(sigma));
+	return sqrt(sigma);
 }
 
 int main() {
@@ -45,6 +68,8 @@ int main() {
 
 		//Длина массива
 		int len;
+		int mint, maxt;
+		float meant, RMSt;
 		printf("\nInput length: ");
 		scanf_s("%d", &len);
 
@@ -54,8 +79,15 @@ int main() {
 			scanf_s("%d", &arr[i]);
 		}
 
-		minax(len);
-		meams(len);
+		maxt = max(len);
+
+		mint = min(len);
+
+		meant = mean(len);
+
+		RMSt = RMS(len);
+
+		printf("Maximum - %d\nMinimum - %d\nMean - %f\nRMS - %f\n", maxt, mint, meant, RMSt);
 
 		//Повтор программы
 		printf("\nDo you wish to restart program? (y/n)\n");
